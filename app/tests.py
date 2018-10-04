8e60c9407a481bb044d76c0e7ae08cd7d55c9736
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-10-04 10:10:41 -0500

    Create initial project using https://github.com/NdagiStanley/vue-django

---
+++ app/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class TestView(TestCase):
+  """Test the views of the application"""
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_index_view(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
