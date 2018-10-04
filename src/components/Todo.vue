8295aae506d9aaa9d70845f2ffde8b4f3bd41ef8
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-10-04 12:26:29 -0500

    Add initial Todo, TodoList, and UserInput components

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+// src/components/Todo.vue
+
+<template>
+  <p>
+    {{ todo.text }}
+  </p>
+</template>
+
+<script>
+  export default {
+    props: ['todo']
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,32 @@
+// src/components/UserInput.vue
+
+<template>
+  <div id="user-inputs">
+    <label for="new-todo">Add a new item:</label>
+    <input id="new-todo" v-model="newTodoText" v-on:keyup.enter="createTodo">
+    <button v-on:click="clearTodos">
+      Clear
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UserInput',
+    data: function () {
+      return {newTodoText: ''}
+    },
+    methods: {
+      createTodo () {
+        console.log(this.newTodoText, 'created!')
+      },
+      clearTodos () {
+        console.log('Todos cleared!')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
