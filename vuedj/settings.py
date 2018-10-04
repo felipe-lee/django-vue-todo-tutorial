01e1c38532285dbb59147e8f58314acea8150467
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-10-04 15:10:26 -0500

    Add rest_framework to project django settings as an installed app

--- vuedj/settings.py
+++ vuedj/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for vuedj project.
 
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+    'rest_framework',
   'app',
   'django_nose',
 ]
