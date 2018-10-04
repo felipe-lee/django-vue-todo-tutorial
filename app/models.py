59253d37c8f16e8db3db717fdeb5debc1767608b
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-10-04 15:24:54 -0500

    Add simple Todo model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Models for app
+"""
 from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+
+
+class Todo(models.Model):
+    """
+    Model to manage to do items.
+    """
+
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Todo: self.text'
